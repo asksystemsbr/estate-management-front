2becd74e43476f72d8d89d3d67b5b6bfa76e78b2
alex_kaam <contato@asksystems.com.br>
2024-08-16 19:20:44 -0300

    -Prod

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 //const apiUrl = 'http://192.168.15.8:5224';//dev
 //const apiUrl = 'http://192.168.0.102:5224'; //dev
-const apiUrl = 'http://localhost:5224'; //dev
+//const apiUrl = 'http://localhost:5224'; //dev
  //const apiUrl = 'http://localhost:4224';//container
-//  const apiUrl = 'https://estatemanagementback.azurewebsites.net';//production
+  const apiUrl = 'https://estatemanagementback.azurewebsites.net';//production
 
 export default apiUrl;  
